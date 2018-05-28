e69baa3535156301d8bbca0344dbb7e545b41712
Henry Zarza <henryacz7@gmail.com>
2018-05-28 12:41:21 -0500

    Creation of components

---
+++ src/pages/Comics/Comics.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Comics extends Component {
+    render() {
+        return (
+            <div>
+                Comics
+            </div>
+        );
+    }
+}
+
+export default Comics;
