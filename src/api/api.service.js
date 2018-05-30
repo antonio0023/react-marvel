0551f1b14a82c9052c91d1c3ff7819647020a102
Henry Zarza <henryacz7@gmail.com>
2018-05-30 17:05:51 -0500

    Beginnig application's structure

--- src/api/api.service.js
+++ src/api/api.service.js
@@ -2,8 +2,8 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://gateway.marvel.com/v1/public/';
 
 const ApiService = () => {
-    const getData = (url, queryParams = {}) => {
-        return fetch(`${BASE_URL}/${url}?limit=1&offset=3&apikey=${API_KEY}`, { method: 'GET' });
+    const getData = (url, queryParams) => {
+        return fetch(`${BASE_URL}/${url}?${queryParams}&apikey=${API_KEY}`, { method: 'GET' });
     }
 
     return ({ getData });
--- src/pages/Characters/Characters.js
+++ src/pages/Characters/Characters.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 import ApiService from '../../api/api.service';
+import Sidebar from '../../components/Sidebar/Sidebar';
+import CharacterInfo from '../../components/CharacterInfo/CharacterInfo';
+import Pagination from '../../components/Pagination/Pagination';
 
 class Characters extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            pagination: {
+                limit: 10
+            },
+            orderBy: 'name'
+        }
+    }
+
     componentDidMount() {
-        ApiService().getData('characters')
+        ApiService().getData('characters', `limit=${this.state.pagination.limit}`)
             .then(response => {
                 if (response.status !== 200) throw new Error('Error');
                 return response.json();
@@ -20,7 +34,10 @@ class Characters extends Component {
     render() {
         return (
             <div>
+                <Sidebar />
                 Characters
+                <CharacterInfo />
+                <Pagination />
             </div>
         );
     }
