d9ec9a06c98d6eb616a9c785bc730fc1b559926f
Henry Zarza <henryacz7@gmail.com>
2018-06-04 18:10:17 -0500

    Style component Card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import './Card.css';
 
 class Card extends Component {
+
+    showInformation = () => {
+        console.log(this.props.data);
+    }
+
     render() {
+        const { data } = this.props;
+        const imgSrc = `${data.thumbnail.path}/standard_large.${data.thumbnail.extension}`;
         return (
-            <div>
-                Card
+            <div className="card" onClick={this.showInformation}>
+                <h3 className="card__title">{data.name}</h3>
+                <img className="card__image" src={imgSrc} alt={data.name}/>
             </div>
         );
     }
