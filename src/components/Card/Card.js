1f972ab3b2b8790b9768436d08bd3778f4fb3695
Henry Zarza <henryacz7@gmail.com>
2018-06-06 10:06:27 -0500

    Correcctions in Card styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,8 +12,8 @@ class Card extends Component {
         const imgSrc = `${data.thumbnail.path}/standard_large.${data.thumbnail.extension}`;
         return (
             <div className="card" onClick={this.showInformation}>
-                <h3 className="card__title">{data.name}</h3>
                 <img className="card__image" src={imgSrc} alt={data.name}/>
+                <h3 className="card__title">{data.name}</h3>
             </div>
         );
     }
