b14aaec8e4ff094cc60045ced9e1d35959a2fc5a
Henry Zarza <henryacz7@gmail.com>
2018-06-08 10:17:46 -0500

    Start styles CharacterInfo component

--- src/components/CharacterInfo/CharacterInfo.js
+++ src/components/CharacterInfo/CharacterInfo.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
+import './CharacterInfo.css';
 
 class CharacterInfo extends Component {
     render() {
         return (
             <div className="character-info">
-                <img src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/standard_large.jpg" alt="Spider-Man"/>
-                <h2 className="character-info__title">spider-man</h2>
+                <div className="character-info__header">
+                    <button className="character-info__button">
+                        <i className="material-icons">clear</i>
+                    </button>
+                    <img className="character-info__image" src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/standard_large.jpg" alt="Spider-Man"/>
+                    <h2 className="character-info__title">spider-man</h2>
+                </div>
+
                 <div className="character-info__content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sapiente perferendis, quas explicabo perspiciatis recusandae, ab voluptatibus consectetur sint sed modi molestiae maxime temporibus autem officiis hic neque nesciunt quis!
                     <div className="character-info__link-content">
@@ -14,6 +21,7 @@ class CharacterInfo extends Component {
                         <a href="http://marvel.com/comics/characters/1009610/spider-man?utm_campaign=apiRef&utm_source=b91712fbe43cb1432019131a90cf2bb7" target="blank">detail</a>
                     </div>
                 </div>
+
                 <div className="character-info__content">
                     <a className="character-info__link" href="http://gateway.marvel.com/v1/public/characters/1009610/stories" target="blank">comics</a>
                     available: 765
