58d8206e207bddd4551b999d844b5701cdfdd2ab
Henry Zarza <henryacz7@gmail.com>
2018-06-08 17:40:57 -0500

    Design character information

--- src/components/CharacterInfo/CharacterInfo.js
+++ src/components/CharacterInfo/CharacterInfo.js
@@ -12,7 +12,6 @@ class CharacterInfo extends Component {
                     <img className="character-info__image" src="http://i.annihil.us/u/prod/marvel/i/mg/3/50/526548a343e4b/standard_large.jpg" alt="Spider-Man"/>
                     <h2 className="character-info__title">spider-man</h2>
                 </div>
-
                 <div className="character-info__content">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio sapiente perferendis, quas explicabo perspiciatis recusandae, ab voluptatibus consectetur sint sed modi molestiae maxime temporibus autem officiis hic neque nesciunt quis!
                     <div className="character-info__link-content">
@@ -21,46 +20,41 @@ class CharacterInfo extends Component {
                         <a href="http://marvel.com/comics/characters/1009610/spider-man?utm_campaign=apiRef&utm_source=b91712fbe43cb1432019131a90cf2bb7" target="blank">detail</a>
                     </div>
                 </div>
-
                 <div className="character-info__content">
                     <a className="character-info__link" href="http://gateway.marvel.com/v1/public/characters/1009610/stories" target="blank">comics</a>
-                    available: 765
-                    <div className="character-info__list">
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                        <div>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</div>
-                    </div>
+                    <h6 className="character-info__available">Available: <span className="emphasize">765</span> </h6>
+                    <ul className="character-info__list">
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                    </ul>
                 </div>
                 <div className="character-info__content">
                     <a className="character-info__link" href="http://gateway.marvel.com/v1/public/characters/1009610/stories" target="blank">stories</a>
-                    available: 765
-                    <div className="character-info__list">
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                        <div>Interior #483 ~~~~ cover</div>
-                    </div>
+                    <h6 className="character-info__available">Available: <span className="emphasize">765</span> </h6>
+                    <ul className="character-info__list">
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                    </ul>
                 </div>
                 <div className="character-info__content">
                     <a className="character-info__link" href="http://gateway.marvel.com/v1/public/characters/1009610/stories" target="blank">series</a>
-                    available: 765
-                    <div className="character-info__list">
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                        <div>A Year of Marvels: April Infinite Comic (2016 - Present)</div>
-                    </div>
+                    <h6 className="character-info__available">Available: <span className="emphasize">765</span> </h6>
+                    <ul className="character-info__list">
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                        <li>Spider-Man: 101 Ways to End the Clone Saga (1997) #1</li>
+                    </ul>
                 </div>
             </div>
         );
